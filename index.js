14414bffa5af6c9420a1b9dce1aed08aeb3f6cdc
Isabel Jenkins <isabel.jenkins@madetech.com>
2021-11-25 12:31:29 +0000

    Started implementing render logic
    Co-authored-by: Bhavesh-Patel <Bhavesh-Patel@users.noreply.github.com>

--- index.js
+++ index.js
@@ -4,17 +4,35 @@ const two = new Two(params).appendTo(elem)
 two.renderer.setSize(800, 600)
 const socket = new WebSocket('ws://localhost:1428')
 
-socket.addEventListener('open', function (event) {
-    socket.send('Hello Server!')
-})
-
-socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data)
-})
-
 let rect
-drawSquare() // First time draw
-window.requestAnimationFrame(loop)
+let playerId
+let action
+let entities = []
+
+setup()
+
+function setup() {
+    drawSquare() // First time draw
+    window.requestAnimationFrame(loop)
+
+    socket.addEventListener('open', function (event) {
+        console.log('Connected to server')
+    })
+    
+    socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data)
+        let jsonData = JSON.parse(event.data)
+        
+        if (jsonData.event == 'startup') {
+            playerId = jsonData.id
+            action = jsonData.action
+        } else if (jsonData.event == 'gameUpdate') {
+            gameUpdate(jsonData.entities)
+        } else {
+            console.log('Unexpceted event: ' + jsonData.event)
+        }
+    })
+}
 
 function drawSquare() {
     rect = two.makeRectangle(100, 100, 100, 100)
@@ -28,4 +46,19 @@ function loop() {
     rect.translation.y++;
     two.update()
     window.requestAnimationFrame(loop)
+}
+
+function gameUpdate(updateEntities) {
+    // remove entities that no longer exist from the server
+    entities = entities.filter(entity1 => updateEntities.some(entity2 => entity2.id === entity1.id))
+
+    updateEntities.forEach(entity => {
+        let foundEntities = entities.filter(x => x.id == entity.id)
+        if (foundEntities.length == 0) {
+            // TODO: create new two.js object
+            entities.push(entity)
+        } else if (foundEntities.length == 1) {
+            // TODO: update existing two.js object
+        }
+    })
 }
